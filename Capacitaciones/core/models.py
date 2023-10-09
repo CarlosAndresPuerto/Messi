7d2a391c05f0ff729ba939f5ec2208f199bbd073
Carlos Puerto <140007935+CarlosAndresPuerto@users.noreply.github.com>
2023-10-09 12:45:29 -0500

    Add files via upload

---
+++ Capacitaciones/core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class UserCustom(AbstractUser):
+
+   identity = models.PositiveIntegerField(
+        verbose_name='Numero de identidad',
+        validators=[MinValueValidator(100000000), MaxValueValidator(9999999999)],
+        unique=True,
+        db_index=True,
+        default=-1
+    )
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
+    created = models.TextField(blank=True, null=True)
+    datecompleted = models.DateTimeField(auto_now_add=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False, null=True)
+    def __str__(self):
+        return self.title + " - " + self.project.name
---
+++ Capacitaciones/core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import register, user_login, profile, projects, tasks, create_task, create_project, project_detail
+
+urlpatterns = [
+    path('register/', register, name="registro"),
+    path('login/', user_login, name='login'),
+    path('profile/', profile, name='profile'),
+    path('projects/', projects, name="projects"),
+    path('projects/<int:id>', project_detail, name="projects_detail"),
+    path('tasks/', tasks, name="tasks"),
+    path('create_task/', create_task,name="create_task"),
+    path("create_project/", create_project, name="create_project"),
+
+
+]
