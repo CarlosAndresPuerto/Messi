3392509ec1aba3744e6c71c317f07847731114df
Carlos Puerto <140007935+CarlosAndresPuerto@users.noreply.github.com>
2023-10-18 14:32:57 -0500

    Delete mysite directory

--- mysite/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import View
-# Create your views here.
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html', context = {})
-
