590de61401d0a9fcb0512e2ca0a655fc702b3ebf
Carlos Puerto <140007935+CarlosAndresPuerto@users.noreply.github.com>
2023-12-13 01:42:49 -0500

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin, messages
-from .models import CustomUser
-
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_student', 'is_teacher')
-
-# Registra la clase CustomUserAdmin
-admin.site.register(CustomUser, CustomUserAdmin)
